16d4dc327518880dff1445a461b0f83c256798b9
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-01-18 20:38:23 +0100

    Add insertion sort support

--- src/sorts.cpp
+++ src/sorts.cpp
@@ -8,3 +8,15 @@ void bubble_sort(DrawableArray& draw_arr) {
         }
     }
 }
+
+void insertion_sort(DrawableArray& draw_arr) {
+    for (size_t i = 1; i < draw_arr.len(); i++) {
+        for (size_t j = i; j > 0; j--) {
+            if (draw_arr.at(j) < draw_arr.at(j - 1)) {
+                draw_arr.swap(j, j - 1);
+            } else {
+                break;
+            }
+        }
+    }
+}
--- src/sorts.hpp
+++ src/sorts.hpp
@@ -1,2 +1,3 @@
 #include <drawable_array.hpp>
 void bubble_sort(DrawableArray& draw_arr);
+void insertion_sort(DrawableArray& draw_arr);
